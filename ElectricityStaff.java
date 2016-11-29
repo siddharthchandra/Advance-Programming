73f68b17a08f2dcae8c2da574fd8cd4fffae6f63
PrasoonJain <prasoon15157@iiitd.ac.in>
2016-11-30 05:05:17 +0530

    Update ElectricityStaff.java

--- ElectricityStaff.java
+++ ElectricityStaff.java
@@ -1,4 +1,4 @@
-mport javax.swing.*;
+import javax.swing.*;
 import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.*;
@@ -110,6 +110,15 @@ public class ElectricityStaff {
                     submit.addActionListener(new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
+                                          if(userfield.getText().length()==0 && flag1==0)
+                {
+                    flag1=1;
+                    JOptionPane.showMessageDialog(jf1, "Enter your Username !");
+                }             if(material.getText().length()==0 && flag==0)
+                {
+                    flag=1;
+                    JOptionPane.showMessageDialog(jf1, "Enter materials !");
+                }
                             logisticsRequestses.add(new LogisticsRequests(userfield.getText().toString(),departmentList.getSelectedItem().toString(),material.getText().toString(),typeList.getSelectedItem().toString()));
                             jFrame.setVisible(false);
 
