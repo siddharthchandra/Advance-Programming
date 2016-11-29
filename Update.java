7d503b8d88543b272acbd522529fcddb4b085019
Siddharth Chandra <siddharth15100@iiitd.ac.in>
2016-11-29 17:40:18 +0530

    Add files via upload

---
+++ Update.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Update {
+    Update(ArrayList<AssignTask> assignTasks,ArrayList<RegistrationRequests> registrationRequestses,ArrayList<Database> databases,ArrayList<AudioVideoStaff> audioVideoStaffs,ArrayList<ElectricityStaff> electricityStaffs,ArrayList<HousekeepingStaff> housekeepingStaffs,ArrayList<HVACStaff> hvacStaffs,ArrayList<SecurityStaff> securityStaffs,ArrayList<DepartmentSupervisors> departmentSupervisorses) throws IOException {
+        //Creating strings of different files of csv
+        String audiovideo = "Audio-Video Staff.csv";
+        String housekeeping="Housekeeping Staff.csv";
+        String security="Security Staff.csv";
+        String hvac="HVAC Staff.csv";
+        String electricity="Electricity Staff.csv";
+        String department="Department Supervisors.csv";
+        String database="Database.csv";
+        String assign="Assigned Tasks.csv";
+        String regrequests="Registration Requests.csv";
+
+    }
+}
